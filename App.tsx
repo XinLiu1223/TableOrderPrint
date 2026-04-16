108da550dc4390eb824e60a487918b058356c8be
XinLiu1223 <xinliu1223@gmail.com>
2026-04-16 15:24:42 -0400

    add drawer and fix dipendency error

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import RegMainForm from "./RegMainScreen";
 import { StyleSheet } from "react-native";
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./HomeScreen";
@@ -19,17 +22,42 @@ type RootStackParamList = {
   Registration: undefined;
 };
 
+const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function Home() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen
+        name="Products"
+        component={StackScreen}
+        options={{ headerShown: false }}
+      />
+    </Drawer.Navigator>
+  );
+}
+
+function StackScreen() {
+  return (
+    <Stack.Navigator>
+      {/* <Stack.Screen name="Home" component={Home} /> */}
+      <Stack.Screen name="Products" component={ProductsScreen} />
+      <Stack.Screen name="Details" component={ProductsDetails} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Products" component={ProductsScreen} />
-        <Stack.Screen name="Details" component={ProductsDetails} />
-        {/* <Stack.Screen name="Registration" component={RegMainForm} /> */}
-      </Stack.Navigator>
+      {Home()}
+      {/* <Stack.Navigator> */}
+      {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+      {/* <Stack.Screen name="Products" component={ProductsScreen} /> */}
+      {/* <Stack.Screen name="Details" component={ProductsDetails} /> */}
+      {/* <Stack.Screen name="Registration" component={RegMainForm} /> */}
+      {/* </Stack.Navigator> */}
       {/* <RegMainForm /> */}
       <StatusBar style="auto" />
     </NavigationContainer>
