49f73b4d243a68ac71bb5932ffc665edcf72c1fc
XinLiu1223 <xinliu1223@gmail.com>
2026-04-30 14:03:17 -0400

    add edit icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,44 @@
-import { Image, View, StyleSheet, useWindowDimensions } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import {
+  Image,
+  View,
+  StyleSheet,
+  useWindowDimensions,
+  Text,
+} from "react-native";
+
+type RootStackParamList = {
+  Profile: undefined;
+};
 
 export default function Header() {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
   return (
     <View style={styles.header}>
       {/* <Text style={styles.text}>Header</Text> */}
       <Image
         source={require("../assets/images/header-background-unspecified.png")}
-        style={styles.header}
+        style={styles.headerImage}
       />
+      <View style={styles.iconTextView}>
+        <View style={styles.textView}>
+          <Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>
+            Alex
+          </Text>
+        </View>
+        <View style={styles.iconView}>
+          <AntDesign
+            name="edit"
+            size={30}
+            color={"white"}
+            onPress={() => navigation.navigate("Profile")}
+          />
+        </View>
+      </View>
       <View style={styles.profileView}>
         {/* <Text style={styles.text}>Header</Text> */}
         <Image
@@ -25,7 +56,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginBottom: 60,
   },
-  text: {},
+  headerImage: {
+    position: "absolute",
+  },
   profileView: {
     width: 100,
     height: 100,
@@ -38,5 +71,18 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 50,
+  },
+  iconTextView: {
+    flexDirection: "row",
+  },
+  textView: {
+    position: "absolute",
+    top: 40,
+    left: "-3%",
+  },
+  iconView: {
+    top: 40,
+    right: -120,
+    position: "relative",
   },
 });
