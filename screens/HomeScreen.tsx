a7a0e9b4453da284cd098fcd1e219089f2b49760
XinLiu1223 <xinliu1223@gmail.com>
2026-05-02 22:56:39 -0400

    add react native element ui

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,9 +3,12 @@ import { StatusBar } from "expo-status-bar";
 
 export default function SocAppHomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.rootView}>
       {/* <StatusBar style="auto" /> */}
+      <View style={styles.profileView}>
+        <Text>Alex</Text>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
     </View>
   );
 }
@@ -14,7 +17,7 @@ const styles = StyleSheet.create({
   rootView: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    // justifyContent: "center",
   },
   textHeader: {
     fontSize: 22,
@@ -25,6 +28,28 @@ const styles = StyleSheet.create({
     width: "90%",
     alignItems: "center",
     marginVertical: 10,
+  },
+  profileView: {
+    alignItems: "center",
+    // justifyContent: "center",
+    height: 50,
+    // borderWidth: 1,
+  },
+  inputContainer: {
+    flexDirection: "column",
+    width: "90%",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  textName: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginVertical: 5,
+  },
+  textDescription: {
+    fontSize: 16,
+    fontWeight: 400,
+    marginVertical: 5,
   },
   input: {
     height: 40,
