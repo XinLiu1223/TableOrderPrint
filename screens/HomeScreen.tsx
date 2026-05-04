f322200ca52ab943bf080a960e99c219831f980e
XinLiu1223 <xinliu1223@gmail.com>
2026-05-03 20:27:00 -0400

    add follow section

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -9,6 +9,16 @@ export default function SocAppHomeScreen() {
         <Text>Alex</Text>
         <Text>Open up App.tsx to start working on your app!</Text>
       </View>
+      <View style={styles.folowerAndFollowing}>
+        <View style={styles.folowerView}>
+          <Text style={styles.textTitle}>Followers</Text>
+          <Text style={styles.followerNumber}>100</Text>
+        </View>
+        <View style={styles.followingView}>
+          <Text style={styles.textTitle}>Following</Text>
+          <Text style={styles.followingNumber}>100</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -17,7 +27,6 @@ const styles = StyleSheet.create({
   rootView: {
     flex: 1,
     alignItems: "center",
-    // justifyContent: "center",
   },
   textHeader: {
     fontSize: 22,
@@ -31,9 +40,38 @@ const styles = StyleSheet.create({
   },
   profileView: {
     alignItems: "center",
-    // justifyContent: "center",
+    height: 30,
+  },
+  folowerAndFollowing: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: "95%",
     height: 50,
-    // borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  folowerView: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  followingView: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  followerNumber: {
+    marginHorizontal: 5,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  followingNumber: {
+    marginHorizontal: 5,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  textTitle: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginVertical: 5,
   },
   inputContainer: {
     flexDirection: "column",
